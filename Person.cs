340674cc9176b8a8b238341b117384188e7444a1
Ben Underwood <javerikr@gmail.com>
2025-11-09 21:26:10 -0500

    Constructors task

--- Person.cs
+++ Person.cs
@@ -2,8 +2,20 @@
 
 public class Person
 {
-    public string? Name;
-    public int Age;
+    private string? Name;
+    private int Age;
+    
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public Person()
+    {
+        Name = "Unknown";
+        Age = 0;
+    }
+
     public void SayHello()
     {
         Console.WriteLine($"Hello, my name is {Name}.");
--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-var person1 = new DotnetMentorshipAssignment2.Person();
+var person1 = new DotnetMentorshipAssignment2.Person("Alice", 30);
 
-person1.Name = "Alice";
-person1.Age = 30;
+// person1.Name = "Alice";
+// person1.Age = 30;
 
 person1.SayHello();
 person1.CelebrateBirthday();
 
 var person2 = new DotnetMentorshipAssignment2.Person();
 
-person2.Name = "Bob";
-person2.Age = 25;
+// person2.Name = "Bob";
+// person2.Age = 25;
 person2.SayHello();
 person2.CelebrateBirthday();
 
